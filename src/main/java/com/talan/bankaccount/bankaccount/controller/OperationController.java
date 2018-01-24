b25fa2ecae4662d1fe5bd24efdab4ae0f518e67b
Abderrazak EL HOU <abderrazak.el-hou@corp.talan.com>
2018-01-24 12:14:45 +0100

    adding failing test depositMoney_validAccount_expect_depositSuccess operationControllerTest

---
+++ src/main/java/com/talan/bankaccount/bankaccount/controller/OperationController.java
@@ -0,0 +1,19 @@
+package com.talan.bankaccount.bankaccount.controller;
+
+import com.talan.bankaccount.bankaccount.domain.Operation;
+import com.talan.bankaccount.bankaccount.util.OperationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OperationController {
+
+    @PostMapping(value = "/deposit", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Operation deposit(@RequestBody OperationDTO operationDTO) {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/test/java/com/talan/bankaccount/bankaccount/Controller/OperationControllerTest.java
@@ -0,0 +1,49 @@
+package com.talan.bankaccount.bankaccount.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.talan.bankaccount.bankaccount.controller.OperationController;
+import com.talan.bankaccount.bankaccount.util.OperationDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyObject;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(OperationController.class)
+public class OperationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+    private OperationDTO operationDTO;
+    @Before
+    public void initialize() {
+        objectMapper = new ObjectMapper();
+        operationDTO = new OperationDTO(123123123L, 1000D);
+
+    }
+
+    @Test
+    public void depositMoney_validAccount_expect_depositSuccess() throws Exception {
+
+        //given(operationService.deposit(anyObject())).willReturn(deposit);
+        mockMvc.perform(MockMvcRequestBuilders.post("/deposit").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(operationDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("account.accountNumber").value(123123123L))
+                .andExpect(jsonPath("account.balance").value(1000D))
+                .andExpect(jsonPath("amount").value(1000D))
+                .andExpect(jsonPath("type").value("DEPOSIT"));
+    }
+}
