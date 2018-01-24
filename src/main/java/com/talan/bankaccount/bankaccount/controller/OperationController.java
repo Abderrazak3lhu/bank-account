225ea53a45c620b388c40859a874ddc4cfb75059
Abderrazak EL HOU <abderrazak.el-hou@corp.talan.com>
2018-01-24 15:41:05 +0100

    success testing depositMoney_NotValidAccount_expect_responseStatusNotAcceptable in operationControllerTest

--- src/main/java/com/talan/bankaccount/bankaccount/controller/OperationController.java
+++ src/main/java/com/talan/bankaccount/bankaccount/controller/OperationController.java
@@ -1,13 +1,13 @@
 package com.talan.bankaccount.bankaccount.controller;
 
 import com.talan.bankaccount.bankaccount.domain.Operation;
+import com.talan.bankaccount.bankaccount.exception.AccountNotFoundException;
 import com.talan.bankaccount.bankaccount.service.OperationService;
 import com.talan.bankaccount.bankaccount.util.OperationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OperationController {
@@ -19,4 +19,9 @@ public class OperationController {
         return operationService.deposit(operationDTO);
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    private void AccountNotFoundHandler(AccountNotFoundException e){
+
+    }
 }
--- src/test/java/com/talan/bankaccount/bankaccount/controller/OperationControllerTest.java
+++ src/test/java/com/talan/bankaccount/bankaccount/controller/OperationControllerTest.java
@@ -3,12 +3,15 @@ package com.talan.bankaccount.bankaccount.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.talan.bankaccount.bankaccount.domain.Account;
 import com.talan.bankaccount.bankaccount.domain.Operation;
+import com.talan.bankaccount.bankaccount.exception.AccountNotFoundException;
+import com.talan.bankaccount.bankaccount.service.AccountService;
 import com.talan.bankaccount.bankaccount.service.OperationService;
 import com.talan.bankaccount.bankaccount.util.OperationDTO;
 import com.talan.bankaccount.bankaccount.util.OperationType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyObject;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,8 +33,12 @@ public class OperationControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+
     @MockBean
     OperationService operationService;
+
+    @MockBean
+    AccountService accountService;
 
     private ObjectMapper objectMapper;
     private OperationDTO operationDTO;
@@ -39,6 +47,7 @@ public class OperationControllerTest {
     @Before
     public void initialize() {
         objectMapper = new ObjectMapper();
+        operationDTO = new OperationDTO(123123123L, 1000D);
         operationDTO = new OperationDTO(123123123L, 1000D);
         accountDeposit = new Account(123123123L, 0);
         accountDeposit.setBalance(accountDeposit.getBalance() + operationDTO.getAmount());
@@ -57,5 +66,14 @@ public class OperationControllerTest {
                 .andExpect(jsonPath("account.balance").value(1000D))
                 .andExpect(jsonPath("amount").value(1000D))
                 .andExpect(jsonPath("type").value("DEPOSIT"));
+    }
+
+    @Test
+    public void depositMoney_NotValidAccount_expect_responseStatusNotAcceptable() throws Exception {
+
+        given(operationService.deposit(anyObject())).willThrow(new AccountNotFoundException());
+        mockMvc.perform(MockMvcRequestBuilders.post("/deposit").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(operationDTO)))
+                .andExpect(status().isNotAcceptable());
     }
 }
