ecd9c489e48ce15258447bf0439b378b34e80a7e
Abderrazak EL HOU <abderrazak.el-hou@corp.talan.com>
2018-01-25 14:28:49 +0100

    failing test for transfert for a valid account in operationServiceTest

--- src/test/java/com/talan/bankaccount/bankaccount/service/OperationServiceTest.java
+++ src/test/java/com/talan/bankaccount/bankaccount/service/OperationServiceTest.java
@@ -6,6 +6,7 @@ import com.talan.bankaccount.bankaccount.domain.Operation;
 import com.talan.bankaccount.bankaccount.exception.AmountNotValidException;
 import com.talan.bankaccount.bankaccount.util.OperationDTO;
 import com.talan.bankaccount.bankaccount.util.OperationType;
+import com.talan.bankaccount.bankaccount.util.TransfertDTO;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,13 +33,18 @@ public class OperationServiceTest {
 
     // deposit setup
     private OperationDTO operationDTO;
-    private OperationDTO notValidOperationDTO;
-
     private Operation deposit;
     private Account accountDeposit;
-
+    // withdraw setup
+    private OperationDTO notValidOperationDTO;
     private Operation withdraw;
     private Account accountWithdraw;
+    // transfert setup
+    private TransfertDTO transfertDTO;
+    private Operation transfert;
+    private Account mainAccount;
+    private Account destinationAccount;
+
 
     @Before
     public void initialize() {
@@ -50,6 +56,11 @@ public class OperationServiceTest {
         // withdraw init
         accountWithdraw = new Account(123123123L, 1000D);
         withdraw = new Operation(accountDeposit, 1000D, OperationType.WITHDRAW);
+        // Transfert init
+        transfertDTO = new TransfertDTO(1L, 2L, 1000D);
+        mainAccount = new Account(1L, 1000D);
+        destinationAccount = new Account(1L, 0D);
+        transfert = new Operation(mainAccount, destinationAccount, 1000D, OperationType.TRANSFERT);
     }
 
     // ######### Deposit #########
@@ -90,5 +101,25 @@ public class OperationServiceTest {
         Assertions.assertThat(withdraw.getAccount().getBalance()).isEqualTo(0D);
         Assertions.assertThat(withdraw.getAmount()).isEqualTo(1000D);
         Assertions.assertThat(withdraw.getType()).isEqualTo(OperationType.WITHDRAW);
+    }
+
+    // ######### Transfert #########
+    @Test
+    public void transfertValidAmount_validAccounts_transfertOperationDone() {
+
+        given(accountService.getAccount(transfertDTO.getMainAccountNumber())).willReturn(mainAccount);
+        given(accountService.getAccount(transfertDTO.getDestinationAccountNumber())).willReturn(destinationAccount);
+        given(accountService.updateAccount(mainAccount)).willReturn(mainAccount);
+        given(accountService.updateAccount(destinationAccount)).willReturn(destinationAccount);
+        given(operationRepository.save(any(Operation.class))).willReturn(transfert);
+
+        transfert = operationService.transfert(transfertDTO);
+
+        Assertions.assertThat(transfert.getAccount().getAccountNumber()).isEqualTo(1L);
+        Assertions.assertThat(transfert.getAccount().getBalance()).isEqualTo(0D);
+        Assertions.assertThat(transfert.getDestinationAccount().getAccountNumber()).isEqualTo(2L);
+        Assertions.assertThat(transfert.getDestinationAccount().getBalance()).isEqualTo(1000D);
+        Assertions.assertThat(transfert.getAmount()).isEqualTo(1000D);
+        Assertions.assertThat(transfert.getType()).isEqualTo(OperationType.TRANSFERT);
     }
 }
