a2981b832f17df98dfd49be7e78b551e535cb151
Abderrazak EL HOU <abderrazak.el-hou@corp.talan.com>
2018-02-05 11:12:06 +0100

    refactoring : adding verify amount and nsf functions

--- src/main/java/com/talan/bankaccount/bankaccount/service/OperationService.java
+++ src/main/java/com/talan/bankaccount/bankaccount/service/OperationService.java
@@ -3,11 +3,11 @@ package com.talan.bankaccount.bankaccount.service;
 import com.talan.bankaccount.bankaccount.dao.OperationRepository;
 import com.talan.bankaccount.bankaccount.domain.Account;
 import com.talan.bankaccount.bankaccount.domain.Operation;
+import com.talan.bankaccount.bankaccount.dto.OperationDTO;
+import com.talan.bankaccount.bankaccount.dto.TransfertDTO;
 import com.talan.bankaccount.bankaccount.exception.AmountNotValidException;
 import com.talan.bankaccount.bankaccount.exception.NotSufficientFunds;
-import com.talan.bankaccount.bankaccount.dto.OperationDTO;
 import com.talan.bankaccount.bankaccount.util.OperationType;
-import com.talan.bankaccount.bankaccount.dto.TransfertDTO;
 import com.talan.bankaccount.bankaccount.util.bankAccountConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,18 +23,19 @@ public class OperationService {
     AccountService accountService;
     @Autowired
     OperationRepository operationRepository;
-
-
+    
     public Operation deposit(OperationDTO operationDTO) {
 
         Account account = accountService.getByAccountNumber(operationDTO.getAccountNumber());
-        if (operationDTO.getAmount() <= 0) {
-            throw new AmountNotValidException(bankAccountConstants.AMOUNT_NOT_VALID);
-        }
+
+        verifyIfAmountIsNotValid(operationDTO.getAmount());
+
         account.setBalance(account.getBalance() + operationDTO.getAmount());
         account = accountService.update(account);
+
         Operation deposit = new Operation(account, operationDTO.getAmount(), OperationType.DEPOSIT);
         deposit = operationRepository.save(deposit);
+
         log.info("deposit saved : ", deposit);
         return deposit;
     }
@@ -43,17 +44,16 @@ public class OperationService {
     public Operation withdraw(OperationDTO operationDTO) {
 
         Account account = accountService.getByAccountNumber(operationDTO.getAccountNumber());
-        if (operationDTO.getAmount() <= 0) {
-            throw new AmountNotValidException(bankAccountConstants.AMOUNT_NOT_VALID);
-        }
-        if (operationDTO.getAmount() > account.getBalance()) {
-            throw new NotSufficientFunds(bankAccountConstants.NOT_SUFFICIENT_FUNDS);
-        }
+
+        verifyIfAmountIsNotValid(operationDTO.getAmount());
+        verifyIfFundsAreNotSufficient(operationDTO.getAmount(), account.getBalance());
 
         account.setBalance(account.getBalance() - operationDTO.getAmount());
         account = accountService.update(account);
+
         Operation withdraw = new Operation(account, operationDTO.getAmount(), OperationType.WITHDRAW);
         withdraw = operationRepository.save(withdraw);
+
         log.info("withdraw saved : ", withdraw);
         return withdraw;
     }
@@ -62,12 +62,10 @@ public class OperationService {
     public Operation transfert(TransfertDTO transfertDTO) {
         Account mainAccount = accountService.getByAccountNumber(transfertDTO.getMainAccountNumber());
         Account destinationAccount = accountService.getByAccountNumber(transfertDTO.getDestinationAccountNumber());
-        if (transfertDTO.getAmount() <= 0) {
-            throw new AmountNotValidException(bankAccountConstants.AMOUNT_NOT_VALID);
-        }
-        if (transfertDTO.getAmount() > mainAccount.getBalance()) {
-            throw new NotSufficientFunds(bankAccountConstants.NOT_SUFFICIENT_FUNDS);
-        }
+
+        verifyIfAmountIsNotValid(transfertDTO.getAmount());
+        verifyIfFundsAreNotSufficient(transfertDTO.getAmount(), mainAccount.getBalance());
+
         mainAccount.setBalance(mainAccount.getBalance() - transfertDTO.getAmount());
         destinationAccount.setBalance(mainAccount.getBalance() + transfertDTO.getAmount());
         mainAccount = accountService.update(mainAccount);
@@ -75,11 +73,25 @@ public class OperationService {
 
         Operation transfert = new Operation(mainAccount, destinationAccount, transfertDTO.getAmount(), OperationType.TRANSFERT);
         transfert = operationRepository.save(transfert);
+
+        log.info("transfert saved : ", transfert);
         return transfert;
     }
 
     public List<Operation> transactionsHistoryForAccountNumber(long accountNumber) {
         accountService.getByAccountNumber(accountNumber);
         return operationRepository.findByAllOperationsByAccountNumber(accountNumber);
+    }
+
+    private void verifyIfAmountIsNotValid(double amount) {
+        if (amount <= 0) {
+            throw new AmountNotValidException(bankAccountConstants.AMOUNT_NOT_VALID);
+        }
+    }
+
+    private void verifyIfFundsAreNotSufficient(double amount, double accountBalance) {
+        if (amount > accountBalance) {
+            throw new NotSufficientFunds(bankAccountConstants.NOT_SUFFICIENT_FUNDS);
+        }
     }
 }
